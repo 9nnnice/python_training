7afab4d4fcb0cf99581965668c01a6b2da07e91a
Kukushka <kostya.devyatkin92@mail.ru>
2021-10-14 20:56:58 +0300

    Задание №3. Добавил описания к полям. Добавил logout ч.3

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,7 +17,9 @@ class test_add_contact(unittest.TestCase):
     def test_test_add_contact(self):
         success = True
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -25,7 +27,9 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open contacts page
         wd.find_element_by_link_text("add new").click()
+        # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Kostyan")
@@ -35,8 +39,12 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys("Testovich")
+        # submit contact creation
         wd.find_element_by_name("submit").click()
+        # return to home page
         wd.find_element_by_link_text("home page").click()
+        # logout
+        wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
 
     def tearDown(self):
