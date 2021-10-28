aad97d0735a1e22ce5804de0ad676b0dce418e29
Kukushka <kostya.devyatkin92@mail.ru>
2021-10-28 22:19:54 +0300

    Задание №5: Реализовать двухуровневую архитектуру тестов
    Итоговый вариант

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 class Application:
 
@@ -9,6 +10,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def destroy(self):
         self.wd.quit()
@@ -16,26 +18,3 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, contact):
-        wd = self.wd
-        self.open_home_page()
-        # init contact creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_home_page()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="Kostyan", middle_name="Testin", last_name="Testovich"))
+    app.contact.create(Contact(first_name="Kostyan", middle_name="Testin", last_name="Testovich"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(first_name="", middle_name="", last_name=""))
+    app.contact.create(Contact(first_name="", middle_name="", last_name=""))
     app.session.logout()
