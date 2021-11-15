725b9117df48fa7aed6cd392e242694120c8529d
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-15 20:12:54 +0300

    Задание №7. Попытка 2. ч.2 (конечный вар.)
    Добавил модификацию групп, мелкие доработки по модификации в контактах

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+import time
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -33,6 +35,21 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+        # init group modification
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name2)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header2)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer2)
+        # submit update group
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def open_groups_page(self):
--- model/group.py
+++ model/group.py
@@ -1,6 +1,9 @@
 class Group:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name, header, footer, name2, header2, footer2):
         self.name = name
         self.header = header
         self.footer = footer
+        self.name2 = name2
+        self.header2 = header2
+        self.footer2 = footer2
