e2d7decd64a07a120b8e4fc40cbdd8dff18d8057
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-16 21:02:01 +0300

    Лекция 3.2 Оптимизация ввода данных в формы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,3 +4,13 @@ def test_modification_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modification(Group(name="First name", header="Second name", footer="Public group"))
     app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
