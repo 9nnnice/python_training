29274af4e645b987f9028729b0e5608b63109042
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-16 18:15:51 +0300

    Лекция 3.2 ч.1
    Добавление '=None' в model к контактам и группам

--- model/contact.py
+++ model/contact.py
@@ -1,11 +1,9 @@
 class Contact:
 
-    def __init__(self, first_name, middle_name, last_name):
+    def __init__(self, first_name=None, middle_name=None, last_name=None, nickname=None, title=None, company=None):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
-
-    def __init__(self, nickname, title, company):
         self.nickname = nickname
         self.title = title
         self.company = company
