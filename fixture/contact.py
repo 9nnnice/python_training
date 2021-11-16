08f41a4b76ee35a7055b0d71b3b25be4c06f4178
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-16 15:51:48 +0300

    Revert "Задание №7. Попытка 2. ч.1"
    This reverts commit 437d279007d1078fc8e260a935c801778510dd8d.

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,7 +7,6 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
 
-
     def create(self, contact):
         wd = self.app.wd
         # init contact creation
@@ -25,22 +24,6 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
-        # init contact modification
-        wd.find_element_by_xpath("//*[@title='Edit']").click()
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        # update contact
-        wd.find_element_by_name("update").click()
-        self.return_to_home_page()
-
-
 
     def delete_first_contact(self):
         wd = self.app.wd
--- model/contact.py
+++ model/contact.py
@@ -1,9 +1,6 @@
 class Contact:
 
-    def __init__(self, first_name, middle_name, last_name, nickname, title, company):
+    def __init__(self, first_name, middle_name, last_name):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
-        self.nickname = nickname
-        self.title = title
-        self.company = company
