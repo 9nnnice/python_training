9e379324bbd254f5b033f3ca78aaaa975c0a9043
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-18 20:33:33 +0300

    Лекция 3.4 Ошибка при убирании app.session.logout()
    в test_add_group.py в тесте "test_add_group"

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -19,9 +19,9 @@ class Application:
         except:
             return False
 
-    def destroy(self):
-        self.wd.quit()
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
