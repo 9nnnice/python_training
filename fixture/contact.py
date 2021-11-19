e12decf929a35d93aef56188dcb8db120dbcf45e
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-19 21:27:48 +0300

    Задание №10 (конечный вариант)
    Оптимизировать переходы между страницами

--- fixture/contact.py
+++ fixture/contact.py
@@ -56,4 +56,5 @@ class ContactHelper:
 
     def open_contacts_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
+            wd.find_element_by_link_text("home").click()
