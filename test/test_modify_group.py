4f5f71c6995b7c2195c1196f1fb566a15397fca8
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-19 17:42:22 +0300

    Задание №9 ч.1
    мелкая правка

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -11,6 +11,7 @@ def test_modify_group_name(app):
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
 
+
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
