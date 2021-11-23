3ef3989416c79e1984e555b4c9abcfc48ff85a25
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-23 17:47:56 +0300

    Лекция 4.2 ч.3
    Проверка равенства объектов и списков

--- geometry.py
+++ geometry.py
@@ -1,21 +1,6 @@
-from geom2d import *
-from math import sqrt
+from geom2d.point import *
 
-class Point:
-    def __init__(self, _x, _y):
-        self.x = _x
-        self.y = _y
+l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
+l2 = [Point(0, 0), Point(1, 2), Point(2, 1)]
 
-    def distance(self, p2):
-        dx = p2.x - self.x
-        dy = p2.y - self.y
-        return sqrt(dx*dx + dy*dy)
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-a = Point(0, 0)
-b = Point(3, 4)
-print(a.distance(b))
-print(a == b)
-print(a == Point(0, 0))
+print(l1 == l2)
