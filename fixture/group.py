cfbd494e8d109cc3514ebf3e7f45580abeddd605
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-25 15:27:31 +0300

    Лекция 4.8 (Загружаем информацию из тестируемого приложения в виде списков)

--- fixture/group.py
+++ fixture/group.py
@@ -11,7 +11,7 @@ class GroupHelper:
         groups = []
         for element in wd.find_elements_by_css_selector("span.group"):
             text = element.text
-            id = element.find_element_ba_name("selected[]").get_attribute("value")
+            id = element.find_element_by_name("selected[]").get_attribute("value")
             groups.append(Group(name=text, id = id))
         return groups
 
@@ -33,18 +33,6 @@ class GroupHelper:
         # fill group form
         self.fill_group_form(new_group_data)
         # submit modification
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
-    def modification(self, group):
-        wd = self.app.wd
-        self.open_groups_page()
-        # init group modification
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_name("edit").click()
-        # modify group form
-        self.fill_group_form(group)
-        # submit group creation
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
