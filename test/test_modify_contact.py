0f9a27187ed0b98049401eec2412aa7a3f62b053
Kukushka <kostya.devyatkin92@mail.ru>
2021-11-30 22:46:44 +0300

    Задание №11 ч.8

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,6 +5,10 @@ def test_modify_contact(app):
     old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
-    app.contact.modify_first_contact(Contact(first_name="Patchfirst", middle_name="Patchmiddle", last_name="Patchlast"))
+    contact = Contact(first_name="Patchfirst", middle_name="Patchmiddle", last_name="Patchlast")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
