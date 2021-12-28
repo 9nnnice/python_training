a97ebd40ec15962e3659210cb88a0f349f67257e
Kukushka <kostya.devyatkin92@mail.ru>
2021-12-28 21:51:09 +0300

    Задание №13 ч.2
    Незначительные правки

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,10 +1,10 @@
 from model.contact import Contact
-
+from random import randrange
 
 def test_modify_contact(app):
-    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
+    old_contacts = app.contact.get_contact_list()
     contact = Contact(first_name="Patchfirst", middle_name="Patchmiddle", last_name="Patchlast")
     contact.id = old_contacts[0].id
     app.contact.modify_first_contact(contact)
