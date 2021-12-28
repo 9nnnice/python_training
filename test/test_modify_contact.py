36706d770f6aaf6c710d2083d795f4831ff61892
Kukushka <kostya.devyatkin92@mail.ru>
2021-12-28 22:53:10 +0300

    Revert " Задание №13 ч.5.1"
    This reverts commit 7ad207ace3ec0c47e18ff84cb2a05239a1e1bdca.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,15 +1,14 @@
 from model.contact import Contact
 from random import randrange
 
-def test_modify_some_contact(app):
+def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
     old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
     contact = Contact(first_name="Patchfirst", middle_name="Patchmiddle", last_name="Patchlast")
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(index, contact)
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[index] = contact
+    old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
