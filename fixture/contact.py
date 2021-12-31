3f574419efd675e290b713349881f5672ff341ca
Kukushka <kostya.devyatkin92@mail.ru>
2021-12-31 12:07:17 +0300

    Задание №13 ч.5.2 (ошибка)

--- fixture/contact.py
+++ fixture/contact.py
@@ -31,11 +31,11 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
 
-    def modify_first_contact(self, new_contact_data):
+    def modify_first_contact(self, new_contact_data, index):
         wd = self.app.wd
         self.open_contacts_page()
         # init contact modification
-        wd.find_element_by_xpath("//*[@title='Edit']").click()
+        wd.find_element_by_xpath("//*[@title='Edit']")[index].click()
         # fill contact form
         self.fill_contact_form(new_contact_data)
         # submit contact modification
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,10 +5,11 @@ def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test"))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(first_name="Patchfirst", middle_name="Patchmiddle", last_name="Patchlast")
-    contact.id = old_contacts[0].id
-    app.contact.modify_first_contact(contact)
+    contact.id = old_contacts[index].id
+    app.contact.modify_first_contact(contact, index)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
