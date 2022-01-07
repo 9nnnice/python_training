e7e45443b034aca762ed18de493a2f2250b2d07c
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-07 16:08:22 +0300

    Revert "Лекция 5.5 (подготовка к лекции - переделка контактов)"
    This reverts commit b55876e060905222bf318f4dbe410828e7a724f1.

--- model/contact.py
+++ model/contact.py
@@ -2,21 +2,17 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.homephone = homephone
-        self.mobilephone = mobilephone
-        self.workphone = workphone
-        self.secondaryphone = secondaryphone
+    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
         self.id = id
 
     def __repr__(self):
-        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s" % (self.id, self.first_name)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id)\
-               and self.firstname == other.firstname and self.lastname == other.lastname
+        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name
 
     def id_or_max(self):
         if self.id:
