b55876e060905222bf318f4dbe410828e7a724f1
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-07 15:59:12 +0300

    Лекция 5.5 (подготовка к лекции - переделка контактов)
    Внесены изменения в model\contact.py

--- model/contact.py
+++ model/contact.py
@@ -2,17 +2,21 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None):
-        self.first_name = first_name
-        self.middle_name = middle_name
-        self.last_name = last_name
+    def __init__(self, firstname=None, lastname=None, homephone=None, mobilephone=None, workphone=None, secondaryphone=None, id=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.secondaryphone = secondaryphone
         self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.first_name)
+        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.first_name == other.first_name
+        return (self.id is None or other.id is None or self.id == other.id)\
+               and self.firstname == other.firstname and self.lastname == other.lastname
 
     def id_or_max(self):
         if self.id:
