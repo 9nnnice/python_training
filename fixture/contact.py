4ad9addf686887dc9e3bb3ddcc7113990f409a5d
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-13 19:37:04 +0300

    Задание №14 ч.3 (подготовка к заданию)

--- fixture/contact.py
+++ fixture/contact.py
@@ -18,8 +18,8 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_xpath("td")
-                firstname = cells[2].text
                 lastname = cells[1].text
+                firstname = cells[2].text
                 id = cells[0].find_element_by_name("selected[]").get_attribute("value")
                 all_phones = cells[5].text
                 self.contact_cache.append(Contact(first_name=firstname, last_name=lastname, id=id, all_phones_from_home_page=all_phones))
