498235b14d28e524fe32912ec20bfa32d3f29e7d
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-13 09:16:11 +0300

    Revert "Задание №14 ч.1 (ОШИБКА)"
    This reverts commit 65c906e7cc20fb0eb33c9b64e81ee1d5639aca4f.

--- model/contact.py
+++ model/contact.py
@@ -3,8 +3,7 @@ from sys import maxsize
 class Contact:
 
     def __init__(self, first_name=None, middle_name=None, last_name=None, id=None,
-                 homephone=None, mobilephone=None, workphone=None, secondaryphone=None,
-                 all_phones_from_home_page=None, all_phones_from_view_page=None):
+                 homephone=None, mobilephone=None, workphone=None, secondaryphone=None, all_phones_from_home_page=None):
         self.first_name = first_name
         self.last_name = last_name
         self.middle_name = middle_name
@@ -14,7 +13,6 @@ class Contact:
         self.secondaryphone = secondaryphone
         self.id = id
         self.all_phones_from_home_page = all_phones_from_home_page
-        self.all_phones_from_view_page = all_phones_from_view_page
 
     def __repr__(self):
         return "%s:%s" % (self.id, self.first_name)
