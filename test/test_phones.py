ce4d968ad92e607b3815dd313928a614fa2d2eaf
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-18 17:35:22 +0300

    Лекция 5 (Незначительные правки)

--- test/test_phones.py
+++ test/test_phones.py
@@ -14,12 +14,12 @@ def test_alldata_on_home_page(app):
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
-#def test_phones_on_contact_view_page(app):
-#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-#    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
 
 
 def clear(s):
