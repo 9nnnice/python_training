ec347eff71d49baa76b2fbbc66b016d2edb89242
Kukushka <kostya.devyatkin92@mail.ru>
2022-01-26 22:15:02 +0300

    Лекция 6.5.2 (Обработка исключений)

--- files.py
+++ files.py
@@ -1,7 +1,12 @@
 import json
 
 f = open("c:/temp/py/config.json")
-res = json.load(f)
-f.close()
+try:
+    res = json.load(f)
+except ValueError as ex:
+    print(ex)
+    res={}
+finally:
+    f.close()
 
 print(res)
