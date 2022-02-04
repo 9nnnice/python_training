c785358759e12f6d82da84d7b91fb10ceac958b5
Kukushka <kostya.devyatkin92@mail.ru>
2022-02-04 21:31:16 +0300

    Лекция 7.3 ч.2 (конечная)

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -5,4 +5,5 @@ def test_group_list(app, db):
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
+    ui_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
