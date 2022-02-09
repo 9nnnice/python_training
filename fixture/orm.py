903987556b4c33fabb1b3c8f1defd5fdcafe9f9e
Kukushka <kostya.devyatkin92@mail.ru>
2022-02-10 00:12:28 +0300

    Лекция 7.7 ч.3 (конечный вариант)

--- fixture/orm.py
+++ fixture/orm.py
@@ -1,7 +1,8 @@
 from pony.orm import *
-from datetime import datetime
+from datetime import date, datetime
 from model.group import Group
 from model.contact import Contact
+from pymysql.converters import decoders
 
 class ORMFixture:
 
@@ -24,12 +25,22 @@ class ORMFixture:
     def __init__(self, host, name, user, password):
         self.db.bind('mysql', host=host, database=name, user=user, password=password, autocommit=True)
         self.db.generate_mapping()
+        sql_debug(True)
 
     def convert_groups_to_model(self, groups):
         def convert(group):
             return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
-        return list (map(convert, groups))
+        return list(map(convert, groups))
 
     @db_session
     def get_group_list(self):
         return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), first_name=contact.firstname, last_name=contact.lastname)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
