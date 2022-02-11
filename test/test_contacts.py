d752f0ecfce654a3fc5186e91c76a8ca5e719426
Kukushka <kostya.devyatkin92@mail.ru>
2022-02-11 16:52:01 +0300

    Задание №21 (конечный вариант)

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,18 +1,15 @@
 import re
 from random import randrange
+from model.contact import Contact
 
-def test_alldata_on_home_page(app):
-    # contact_from_home_page = app.contact.get_contact_list()[0]
-    _list = app.contact.get_contact_list()
-    index = randrange(len(_list))
-    contact_from_home_page = _list[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
-    assert contact_from_home_page.last_name == contact_from_edit_page.last_name
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
+def test_alldata_on_home_page(app, db):
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
+    # assert contacts_from_home_page.last_name == contacts_from_db.last_name
+    # assert contacts_from_home_page.address == contacts_from_db.address
+    # assert contacts_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db)
+    # assert contacts_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db)
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
