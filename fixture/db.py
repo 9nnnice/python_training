df4f9b3e8cc3b850f8978b51efc55134376c3d99
littledoomer <xsoft.set@gmail.com>
2022-02-23 05:46:08 +0300

    Задание №21 (конечный вариант)

--- fixture/db.py
+++ fixture/db.py
@@ -3,6 +3,7 @@ import pymysql.cursors
 from model.group import Group
 from model.contact import Contact
 
+
 class DbFixture:
 
     def __init__(self, host, name, user, password):
@@ -10,16 +11,19 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+        self.connection = pymysql.connect(
+            host=host, database=name, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            cursor.execute(
+                "select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=str(id), name=re.sub(' +', ' ', name).strip()))
+                list.append(
+                    Group(id=str(id), name=re.sub(' +', ' ', name).strip()))
         finally:
             cursor.close()
         return list
@@ -28,13 +32,32 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute(
+                "select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), first_name=firstname, last_name=lastname))
+                list.append(
+                    Contact(id=str(id), first_name=firstname, last_name=lastname))
         finally:
             cursor.close()
         return list
+
+    def get_contact_by_id(self, id):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            query = "select id, firstname, lastname, address, home, mobile, work, fax, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00' and id=%s" % id
+            cursor.execute(query)
+            for row in cursor:
+                (id, firstname, lastname, address, home,
+                 mobile, work, fax, email, email2, email3) = row
+                list.append(Contact(id=str(id), first_name=firstname,
+                            last_name=lastname, address=address, homephone=home,
+                            mobilephone=mobile, workphone=work, secondhomephone=fax,
+                            email=email, email2=email2, email3=email3))
+        finally:
+            cursor.close()
+        return list[0]
 
     def destroy(self):
         self.connection.close()
--- test/test_contacts.py
+++ test/test_contacts.py
@@ -1,27 +1,20 @@
-import re
-from random import randrange
 from model.contact import Contact
+from random import randrange
+import re
 
 
 def test_alldata_on_home_page(app, db):
     contacts_from_home_page = app.contact.get_contact_list()
-    contacts_from_db = db.get_contact_list()
 
-    for db_contact in contacts_from_db:
-
-        # assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
-        # assert contacts_from_home_page.last_name == contacts_from_db.last_name
-        # assert contacts_from_home_page.address == contacts_from_db.address
-        # assert contacts_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db)
-        # assert contacts_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db)
-
-
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+    for ui_contact in contacts_from_home_page:
+        db_contact = db.get_contact_by_id(ui_contact.id)
+        assert ui_contact.first_name == db_contact.first_name
+        assert ui_contact.last_name == db_contact.last_name
+        assert ui_contact.address == db_contact.address
+        assert ui_contact.all_phones_from_home_page == merge_phones_like_on_home_page(
+            db_contact)
+        assert ui_contact.all_emails_from_home_page == merge_emails_like_on_home_page(
+            db_contact)
 
 
 def clear(s):
