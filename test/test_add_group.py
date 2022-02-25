581f6d8e31603f84085699d4dd12942127e02ea4
Kukushka <kostya.devyatkin92@mail.ru>
2022-02-25 07:18:25 +0300

    Задание №29 (конечный вариант)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import allure
 
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
 
-    old_groups = db.get_group_list()
-    old_groups.append(group)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
 
-    app.group.create(group)
+    with allure.step("When i add group %s to the list" % group):
+        app.group.create(group)
 
-    new_groups = db.get_group_list()
-
-    assert len(old_groups) == len(new_groups)
-
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(
-        new_groups, key=Group.id_or_max)
-
-    if check_ui:
-        sorted(app.group.get_group_list(), key=Group.id_or_max) == sorted(
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(
             new_groups, key=Group.id_or_max)
+        if check_ui:
+            sorted(app.group.get_group_list(), key=Group.id_or_max) == sorted(
+                new_groups, key=Group.id_or_max)
